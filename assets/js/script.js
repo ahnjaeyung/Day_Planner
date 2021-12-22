59cc8f99346c1fc69485426d1b9dd0cd7ca1f75a
ahnjaeyung <ahn.jaeyung@gmail.com>
2021-12-22 17:17:13 -0600

    added save functionality to save button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,17 @@
+var savedPlans = Object.keys(localStorage);
 var saveBtn = $(".saveBtn");
+
 saveBtn.on("click", function() {
-    console.log("hello");
-});
+    // console.log(this);
+    var taskInput = $(this).siblings(".description").val();
+    var timeSlot = $(this).parent().attr("id");
+    localStorage.setItem(timeSlot, taskInput);
+}); // end of saveBtn click event
+
+for (i = 0; i < savedPlans.length; i++) {
+    var taskInput = localStorage.getItem(savedPlans[i]);
+    var savedText = $("#" + savedPlans[i]).find("textarea")
+    savedText.val(taskInput);
+} // end of for loop
+
+$("#currentDay").text(moment().format("LLL"));
